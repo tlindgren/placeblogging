5eb9af4401e27fdbbdb09dee8f6f12e2e691f996
Tim Lindgren <contact@timlindgren.com>
2015-03-08 21:19:17 -0400

    Commit message on grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function (grunt) {
               dir: 'build',
               commit: true,
               push: true,
-              message: '%sourceCommit%'
+              message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
             },
             pages: {
               options: {
